ad32644c97355c5ff4bb98e63143bab6c715cb2f
Nick  Sword <nicksword@hotmail.com>
2020-09-26 09:18:09 +0800

    Updates

---
+++ precache-manifest.7370d0f531cb7996aae11538190ebb4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8716d76b0ba84a0b51223a82cf2183ab",
+    "url": "/React-employee-directory/index.html"
+  },
+  {
+    "revision": "13d85fd23742d425577e",
+    "url": "/React-employee-directory/static/css/main.9d6d39f2.chunk.css"
+  },
+  {
+    "revision": "fc291455d35dd9a00aab",
+    "url": "/React-employee-directory/static/js/2.41f6dc8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-employee-directory/static/js/2.41f6dc8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13d85fd23742d425577e",
+    "url": "/React-employee-directory/static/js/main.93fbf79a.chunk.js"
+  },
+  {
+    "revision": "5b10bc2b53951b80383a",
+    "url": "/React-employee-directory/static/js/runtime-main.a40bf67e.js"
+  }
+]);
